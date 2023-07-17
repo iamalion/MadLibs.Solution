d3c08cb6341510f3dc902a722228f8501706dd71
Lindsay Warr <iamalion@gmail.com>
2023-07-17 16:38:07 -0700

    Fix typos

--- MadLibs/Controllers/HomeController.cs
+++ MadLibs/Controllers/HomeController.cs
@@ -18,10 +18,25 @@ namespace MadLibs.Controllers
         }
 
         [Route("/adventure-story")]
-        public ActionResult Story(string noun)
+        public ActionResult AdventureStory(string animal, string animalName, string city, string adjectiveOne, string objectOne, string emotion, string pluralAnimal, string pluralNounOne, string pluralNounTwo, string numberOne, string occupation, string objectTwo, string numberTwo, string adjectiveTwo, string pluralNounThree)
         {
             MadLibsAdventure myMadLibsAdventure = new MadLibsAdventure();
             myMadLibsAdventure.Animal = animal;
+            myMadLibsAdventure.AnimalName = animalName;
+            myMadLibsAdventure.City = city;
+            myMadLibsAdventure.AdjectiveOne = adjectiveOne;
+            myMadLibsAdventure.ObjectOne = objectOne;
+            myMadLibsAdventure.Emotion = emotion;
+            myMadLibsAdventure.PluralAnimal = pluralAnimal;
+            myMadLibsAdventure.PluralNounOne = pluralNounOne;
+            myMadLibsAdventure.PluralNounTwo = pluralNounTwo;
+            myMadLibsAdventure.NumberOne = numberOne;
+            myMadLibsAdventure.Occupation = occupation;
+            myMadLibsAdventure.ObjectTwo = objectTwo;
+            myMadLibsAdventure.NumberTwo = numberTwo;
+            myMadLibsAdventure.AdjectiveTwo = adjectiveTwo;
+            myMadLibsAdventure.PluralNounThree = pluralNounThree;
+            
             return View(myMadLibsAdventure);
         }
     }
--- MadLibs/Models/MadLibsAdventure.cs
+++ MadLibs/Models/MadLibsAdventure.cs
@@ -2,7 +2,23 @@ namespace MadLibs.Models
 {
     public class MadLibsAdventure
     {
-        public string Noun { get; set; }
+        public string Animal { get; set; }
+        public string AnimalName { get; set; }
+        public string City { get; set; }
+        public string AdjectiveOne { get; set; }
+        public string ObjectOne { get; set; }
+        public string Emotion { get; set; }
+        public string PluralAnimal { get; set; }
+        public string PluralNounOne { get; set; }
+        public string PluralNounTwo { get; set; }
+        public string NumberOne { get; set; }
+        public string Occupation { get; set; }
+        public string ObjectTwo { get; set; }
+        public string NumberTwo { get; set; }
+        public string AdjectiveTwo { get; set; }
+        public string PluralNounThree { get; set; }
+
+
         
     }
 }
